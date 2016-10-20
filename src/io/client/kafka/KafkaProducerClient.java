3fdd6bb630b96119beb99a389baf257232a9bf2c
Ndungu, Kaniu <kaniu.ndungu@neustar.biz>
2016-10-19 19:07:31 -0700

    Made KafkaProducer use generic types for persisted messages

--- src/io/client/kafka/KafkaProducerClient.java
+++ src/io/client/kafka/KafkaProducerClient.java
@@ -17,20 +17,22 @@ import java.util.concurrent.Future;
 
 /**
  * Sends messages to specified topic in producer.props resource
+ * @param <T>
+ * @param <T>
  */
-public class KafkaProducerClient{
+public class KafkaProducerClient<T>{
 	private  static final Logger log = Logger.getLogger(KafkaProducerClient.class);
 	
-	private static KafkaProducerClient dsproducer = null;
+	private static KafkaProducerClient<?> dsproducer = null;
 
-	public static KafkaProducerClient singleton() throws IOException {
+	public static KafkaProducerClient<?> singleton() throws IOException {
 		if (dsproducer == null) {
 			dsproducer = new KafkaProducerClient();
 		}
 		return dsproducer;
 	}
 
-	private KafkaProducer<String, Object> producer = null;
+	private KafkaProducer<String, T> producer = null;
 	private String topic = null;
 	private Properties properties = null;
 
@@ -38,46 +40,30 @@ public class KafkaProducerClient{
 		InputStream props = Resources.getResource("producer.props").openStream();
 		properties = new Properties();
 		properties.load(props);
-		producer = new KafkaProducer<String, Object>(properties);
+		producer = new KafkaProducer<String, T>(properties);
 
 	}
 
-	public Future<RecordMetadata> send(String message) {
+	public Future<RecordMetadata> send(T message) {
 
 		topic = properties.getProperty("topic.id");
-		Future<RecordMetadata> threadFuture = producer.send(new ProducerRecord<String, Object>(topic, message));
+		Future<RecordMetadata> threadFuture = producer.send(new ProducerRecord<String, T>(topic, message));
 		
 		return threadFuture;
 
 	}
 
-	public Future<RecordMetadata> send(String message, String topic, Future<RecordMetadata> type) {
+	public Future<RecordMetadata> send(T message, String topic, Future<RecordMetadata> type) {
 
-		Future<RecordMetadata> threadFuture = producer.send(new ProducerRecord<String, Object>(topic, message));
+		Future<RecordMetadata> threadFuture = producer.send(new ProducerRecord<String, T>(topic, message));
 
 		return threadFuture;
 
 	}
 	
-	public String send(Message message, String topic) {
+	public String send(T message, String topic) {
 
-		Future<RecordMetadata> threadFuture = producer.send(new ProducerRecord<String, Object>(topic, message));
-		String response = (String)"null";
-		try {
-			response =  (String) ("Response = " +threadFuture.get().toString());;
-			log.info(response);
-		} catch (Exception e) {
-			log.error(e,e);
-		}
-		
-		return response;
-
-	}
-	
-
-	public String send(String message, String topic) {
-
-		Future<RecordMetadata> threadFuture = producer.send(new ProducerRecord<String, Object>(topic, message));
+		Future<RecordMetadata> threadFuture = producer.send(new ProducerRecord<String, T>(topic, message));
 		String response = (String)"null";
 		try {
 			response =  (String) ("Response = " +threadFuture.get().toString());;
